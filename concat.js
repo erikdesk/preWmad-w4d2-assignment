10baed1121065ad5da48f1e4b8c7c1b4b9561dae
Erik Desk <desk@erikdesk.lan>
2020-12-16 06:36:11 +0800

    updated assignment with solution code

--- concat.js
+++ concat.js
@@ -1,15 +1,28 @@
 // Create a function to concatenate two integer arrays. it will take in two arrays, 
 // and return one new one.
 
+const concat = function (arr1, arr2) {
+    
+    let newArray = [];
+    
+    for (let i = 0; i < arr1.length; i++) {
+    	newArray.push(arr1[i]);
+    }
+    
+    for (let i = 0; i < arr2.length; i++) {
+    	newArray.push(arr2[i]);
+    }
+
+    return newArray;
+}
 
 
 
-
-concat([1, 3, 5], [2, 6, 8]) 
+console.log(concat([1, 3, 5], [2, 6, 8]));
 // ➞ [1, 3, 5, 2, 6, 8]
 
-concat([7, 8], [10, 9, 1, 1, 2]) 
+console.log(concat([7, 8], [10, 9, 1, 1, 2]));
 // ➞ [7, 8, 10, 9, 1, 1, 2]
 
-concat([4, 5, 1], [3, 3, 3, 3, 3]) 
+console.log(concat([4, 5, 1], [3, 3, 3, 3, 3]));
 // ➞ [4, 5, 1, 3, 3, 3, 3, 3]
--- countOnes.js
+++ countOnes.js
@@ -1,28 +1,41 @@
 // Create a function to count the number of 1s in a 2D array.
 
+const countOnes = function (nestedArr) {
+    
+    let count = 0;
+    
+	for (let i = 0; i < nestedArr.length; i++) {
+	    for (let j = 0; j < nestedArr[i].length; j++) {
+	        if (nestedArr[i][j] === 1) {
+	        	count++;
+	        }
+	    }
+	}
+
+    return count;
+}
 
 
 
-
-countOnes([
+console.log(countOnes([
     [1, 0],
     [0, 0]
-])
+]));
 
 // should return 1
 
-countOnes([
+console.log(countOnes([
     [1, 1, 1],
     [0, 0, 1],
     [1, 1, 1]
-])
+]));
 
 // should return 7
 
-countOnes([
+console.log(countOnes([
     [1, 2, 3],
     [0, 2, 1],
     [5, 7, 33]
-])
+]));
 
 //should return 2
